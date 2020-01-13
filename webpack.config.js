4408fd5c0ad95ecaa391c4a91d3dced3b76fa683
Tikhaya Anastasiia <tikhaya.a.v@gmail.com>
2020-01-14 01:53:33 +0200

    Add style guide.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                      loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                      loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                      loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
+            }
+        ]
+    },
+    watch: true
+}
